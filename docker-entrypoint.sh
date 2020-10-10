a660a2693393268638092467d0d083edccffb973
Naba Kumar Das <67466982+nabad600@users.noreply.github.com>
2020-10-10 21:44:53 +0530

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-echo "upstream php-upstream { server ${CONTAINER_NAME}:${PORT}; }" > /etc/nginx/conf.d/upstream.conf \
-    && rm /etc/nginx/conf.d/default.conf
+echo "upstream php-upstream { server ${CONTAINER_NAME}:${PORT}; }" > /etc/nginx/conf.d/upstream.conf
 
 exec "$@"

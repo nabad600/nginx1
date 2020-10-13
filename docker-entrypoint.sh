8d841887e24bf30a64b7046aeca01fadcfe31846
Naba Kumar Das <67466982+nabad600@users.noreply.github.com>
2020-10-13 12:50:57 +0530

    Auto update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-set -e
+set -eo pipefail
 
 echo "upstream php-upstream { server ${CONTAINER_NAME}:${PORT}; }" > /etc/nginx/conf.d/upstream.conf
+
+initialize_system() {
+    root=${root:-/var/www}
+}
+
+# configure default value file
+sed 's,{{root}},'"${root}"',g' -i /etc/nginx/sites-available/default.conf
 
 exec "$@"
